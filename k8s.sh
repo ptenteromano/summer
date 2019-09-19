e075a64d1c93f0b697064e7ba7515851bcca4520
zsl3203 <zsl3203@gmail.com>
2019-09-19 15:56:44 -0400

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -53,6 +53,8 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+#kubectl apply -f "https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml"
+
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 kubectl get pods --all-namespaces
