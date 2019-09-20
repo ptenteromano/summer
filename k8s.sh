38feffdc9edfa125b848638d702c72245e859f31
zsl3203 <zsl3203@gmail.com>
2019-09-20 13:23:31 -0400

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -51,9 +51,8 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-#kubectl apply -f "https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml"
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/canal.yaml
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
