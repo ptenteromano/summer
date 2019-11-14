ace00ffce8a55d78449a58e69ce8b3ed9faf49fc
Philip Tenteromano <tenteromano.p@gmail.com>
2019-11-14 16:36:11 -0500

    my_ip alias

--- add_alias.sh
+++ add_alias.sh
@@ -1,4 +1,5 @@
 echo "alias k='kubectl'" > ~/.bash_aliases
+echo "alias my_ip=`echo dig +short myip.opendns.com @resolver1.opendns.com`" >> ~/.bash_aliases
 
 # Reloads bash without logging out
 source ~/.bashrc
