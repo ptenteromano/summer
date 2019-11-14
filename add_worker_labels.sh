70275c7b042e7f6adca64ae6c1eff8f7d797c7d7
Philip Tenteromano <tenteromano.p@gmail.com>
2019-11-14 15:01:51 -0500

    config bash programs

---
+++ add_worker_labels.sh
@@ -0,0 +1,3 @@
+kubectl label node worker-1.phil-cap.shield-pg0.utah.cloudlab.us node-role.kubernetes.io/worker=worker;
+kubectl label node worker-2.phil-cap.shield-pg0.utah.cloudlab.us node-role.kubernetes.io/worker=worker;
+kubectl label node worker-3.phil-cap.shield-pg0.utah.cloudlab.us node-role.kubernetes.io/worker=worker;
